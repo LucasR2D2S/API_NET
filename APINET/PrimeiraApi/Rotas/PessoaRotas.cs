4d3930209be7901d9085b4480f96f8b586c72343
Lucas Santos <lucasrrfsantos1@gmail.com>
2024-08-06 11:24:11 -0300

    Subindo projeto

---
+++ APINET/PrimeiraApi/Rotas/PessoaRotas.cs
@@ -0,0 +1,51 @@
+﻿using PrimeiraApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraApi.Rotas
+{
+    public static class PessoaRotas
+    {
+        public static List<Pessoa> Pessoas =
+        [
+            new(Guid.NewGuid(), "Lucas"),
+            new(Guid.NewGuid(), "Yuri Alberto"),
+            new(Guid.NewGuid(), "Thor")
+        ];
+
+        public static void MapPessoaRotas(this WebApplication app) 
+        {
+            app.MapGet("/pessoas", () => Pessoas );
+
+            app.MapGet("/pessoa/{nome}", 
+                (string nome)=>  Pessoas.Find(x => x.Nome == nome));
+
+            app.MapPost("/pessoas", 
+                (HttpContext request, Pessoa pessoa) =>
+                {
+                    //if (pessoa.Nome != "Lucas")
+                    //    return Results.BadRequest(new {message = "Erro pq não é Lucas!"});
+
+                    var nome = request.Request.Query["name"]
+;
+                    Pessoas.Add(pessoa);
+                    return Results.Ok(pessoa);
+
+                });
+
+            app.MapPut("/pessoas/{id:guid}", (Guid id, Pessoa pessoa) =>
+                {
+                    var encontrado = Pessoas.Find(x => x.Id == id);
+
+                    if (encontrado == null)
+                        return Results.NotFound();
+
+                    encontrado.Nome = pessoa.Nome;
+                    
+                    return Results.Ok(encontrado);
+                });
+
+            
+        }
+
+    }
+}
